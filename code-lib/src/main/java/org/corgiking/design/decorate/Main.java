6e6eb48327d5da31b4ee64655a65b45652044eed
yy <2572836523@qq.com>
2017-09-06 17:31:50 +0800

    添加描述

---
+++ code-lib/src/main/java/org/corgiking/design/decorate/Main.java
@@ -0,0 +1,22 @@
+package org.corgiking.design.decorate;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+	
+	/**
+	 * 装饰设计模式:
+	 * 1.透明装饰模式
+	 * 2.半透明装饰模式
+	 * 
+	 * 例子:
+	 * 1.BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("file"))));
+	 * 2.awt,swing中图形界面
+	 * 
+	 * @throws FileNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		
+	}
+
+}
---
+++ code-lib/src/main/java/org/corgiking/design/proxy/Main.java
@@ -0,0 +1,15 @@
+package org.corgiking.design.proxy;
+
+public class Main {
+
+	/**
+	 * 代理设计模式:
+	 * 1.动态代理
+	 * 2.静态代理
+	 * 
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}

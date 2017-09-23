5acebb18c1ac7cfa1b835f8dc087e5d358616ada
yy <2572836523@qq.com>
2017-09-23 20:46:17 +0800

    添加笔记

--- CutInSpring/src/main/java/org/corgiking/Main.java
+++ CutInSpring/src/main/java/org/corgiking/Main.java
@@ -2,7 +2,14 @@ package org.corgiking;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+/**
+ * xsd文件是XML结构定义文件( XML Schemas Definition )
+ * 
+ * spring会加载META-INF目录下的spring.handlers,spring.schemas文件
+ *     spring.schemas文件定义了XML中的标签
+ *     spring.handlers文件制定了该命名空间的解析类
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
@@ -14,3 +21,4 @@ public class Main {
 	}
 
 }
+

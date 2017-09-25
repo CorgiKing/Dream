9eded887969187ed59a6b36b8d56843ce01b4539
yy <2572836523@qq.com>
2017-09-25 09:33:58 +0800

    修改bug

--- code-lib/src/main/java/org/corgiking/xml/demo/Main.java
+++ code-lib/src/main/java/org/corgiking/xml/demo/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		String path ="src/main/resources/org/corgiking/xml/";
 		BeanFactory factory = new BeanFactory(path+"beans.xml");
 		
-		Object user = factory.getBean("users");
+		Object user = factory.getBean("user");
 		System.out.println(user);
 		
 	}

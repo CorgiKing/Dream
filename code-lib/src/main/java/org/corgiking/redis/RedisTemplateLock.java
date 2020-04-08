38c2b60c40e1c8dc87b0084221b16e554178fb43
corgiking <1030185489@qq.com>
2020-04-08 11:05:05 +0800

    redisTemplate分布式锁修改

--- code-lib/src/main/java/org/corgiking/redis/RedisTemplateLock.java
+++ code-lib/src/main/java/org/corgiking/redis/RedisTemplateLock.java
@@ -7,6 +7,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.TimeoutUtils;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
@@ -15,20 +16,6 @@ import redis.clients.util.SafeEncoder;
 
 public class RedisTemplateLock {
 
-	/**
-	 * 过期参数，后跟过期的秒数
-	 */
-	private static final String PARAM_EX = "EX";
-
-	/**
-	 * 不存在该key才会执行set操作
-	 */
-	private static final String PARAM_NX = "NX";
-
-	/**
-	 * 执行成功返回值
-	 */
-	private static final String LOCK_SUCCESS = "OK";
 
 	/**
 	 * 
@@ -39,22 +26,9 @@ public class RedisTemplateLock {
 	 * @param unit          时间单位
 	 * @return
 	 */
-	public static boolean lock(RedisTemplate<String, String> redisTemplate, String lockKey, String lockValue,
-			long timeout, TimeUnit unit) {
-		return redisTemplate.execute(new RedisCallback<Boolean>() {
-
-			@Override
-			public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
-
-				Object ret = connection.execute("set", SafeEncoder.encodeMany(lockKey, lockValue, PARAM_EX,
-						String.valueOf(TimeoutUtils.toSeconds(timeout, unit)), PARAM_NX));
-
-				if (ret != null && LOCK_SUCCESS.equals(SafeEncoder.encode((byte[]) ret))) {
-					return true;
-				}
-				return false;
-			}
-		});
+	public static boolean lock(StringRedisTemplate redisTemplate, String lockKey, String lockValue,
+							   long timeout, TimeUnit unit) {
+		return redisTemplate.opsForValue().setIfAbsent(lockKey, lockValue, timeout, unit);
 	}
 
 	/**
@@ -62,19 +36,22 @@ public class RedisTemplateLock {
 	 * 实现谁加的分布式锁谁才能释放。
 	 * 
 	 * @param redisTemplate
-	 * @param lockKey		分布式锁的key
-	 * @param lockValue		分布式锁中的value符合才能成功释放
+	 * @param unlockKey		分布式锁的key
+	 * @param unlockValue		分布式锁中的value符合才能成功释放
 	 * @return
 	 */
-	public static boolean unLock(RedisTemplate<String, String> redisTemplate, String unlockKey, String unlockValue) {
+	public static boolean unLock(StringRedisTemplate redisTemplate, String unlockKey, String unlockValue) {
 
-		String luaScript = "if redis.call('get', KEYS[1]) == ARGV[1] "
-				+ " then return redis.call('del', KEYS[1]) else return 0 end";
-		RedisScript<Integer> redisScript = new DefaultRedisScript<>(luaScript);
-		
-		Integer ret = redisTemplate.execute(redisScript, Collections.singletonList(unlockKey), Collections.singletonList(unlockValue));
+		String luaScript = "if redis.call('get', KEYS[1]) == ARGV[1]  then " +
+				"return redis.call('del', KEYS[1]) " +
+				"else " +
+				"return 0 " +
+				"end";
+		RedisScript<Long> redisScript = new DefaultRedisScript<>(luaScript, Long.class);
 
-		if (ret.equals(1)) {
+		Long ret = redisTemplate.execute(redisScript, Collections.singletonList(unlockKey), unlockValue);
+
+		if (ret.equals(1L)) {
 			return true;
 		}
 		return false;
